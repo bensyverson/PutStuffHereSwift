cb50af07e78c535c1054fe34fcab9ca964d64228
Ben Syverson <ben@bensyverson.com>
2016-03-28 17:41:10 -0500

    Package typo

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
     name: "PutStuffHere",
 	dependencies: [
-		.Package(url: "https://github.com/IBM-Swift/Kitura-TemplateEngine", majorVersion: 0, minor: 0)
+		.Package(url: "https://github.com/IBM-Swift/Kitura-TemplateEngine.git", majorVersion: 0, minor: 0)
 	]
 )
