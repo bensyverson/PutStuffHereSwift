9648e832e6b81aca6e40af04dd8da868772f730a
Ben Syverson <ben@bensyverson.com>
2016-03-28 17:29:46 -0500

    Update project name. Whoops!

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "PutStuffHereSwift",
+    name: "PutStuffHere",
 	dependencies: [
 		.Package(url: "https://github.com/IBM-Swift/Kitura-TemplateEngine", majorVersion: 0, minor: 0)
 	]
